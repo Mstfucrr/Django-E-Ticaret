850212537d061218d0dafcdab522f940a322bb7d
Mustafa Uçar <mustafa19071907mustafa@gmail.com>
2021-08-15 03:28:46 +0300

    promo section silindi

--- home/views.py
+++ home/views.py
@@ -27,15 +27,11 @@ def index(request):
     womenCategory = Category.objects.get(title = "Kadın")
     slideritems = Product.objects.order_by('?')[:4]
 
-    promoitems = Category.objects.order_by('?')[:4]
-    promooneitem = Product.objects.order_by('?')[:1]
     context['manCategory'] = manCategory
     context['womenCategory'] = womenCategory
     context['manProducts'] = manProducts
     context['womenProducts'] = womenProducts
     context['slideritems'] = slideritems
-    context['promoitems'] = promoitems
-    context['promooneitem'] = promooneitem
 
     return render(request,'index.html',context)
 
