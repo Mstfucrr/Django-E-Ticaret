2d0ac195b4f20b7222f022fd82ab4c8b9aacd8f1
Mustafa Uçar <mustafa19071907mustafa@gmail.com>
2023-04-07 00:42:13 +0300

    fixes

--- order/models.py
+++ order/models.py
@@ -1,6 +1,7 @@
-from django.db import models
 from django.contrib.auth.models import User
+from django.db import models
 from sqlalchemy import false
+
 from product.models import Product
 from user.models import UserProfile
 
