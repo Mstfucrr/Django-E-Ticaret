6125de372d70380a2827775e1d7f695f565c1a93
Mustafa Uçar <mustafa19071907mustafa@gmail.com>
2023-06-21 03:34:32 +0300

    add wishlist to admin panel

--- order/admin.py
+++ order/admin.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
 
-from order.models import Order, OrderItem, ShippingAddress, ShopCart
+from order.models import Order, OrderItem, ShippingAddress, ShopCart, Wishlist
 
 # Register your models here.
 
 class ShopCartAdmin(admin.ModelAdmin):
     list_display = ['customer','product','quantity','amount']
+
+class WishlistAdmin(admin.ModelAdmin):
+    list_display = ['customer','product']
 
 class OrderAdmin(admin.ModelAdmin):
     list_display = ['created_at','customer','status','get_cart_total','get_cart_items']
@@ -17,6 +20,7 @@ class ShippingAdmin(admin.ModelAdmin):
     list_display = ['customer','city','address','country','state','date_added']
 
 admin.site.register(ShopCart,ShopCartAdmin)
+admin.site.register(Wishlist,WishlistAdmin)
 admin.site.register(Order,OrderAdmin)
 admin.site.register(ShippingAddress,ShippingAdmin)
 admin.site.register(OrderItem,OrderItemAdmin)
