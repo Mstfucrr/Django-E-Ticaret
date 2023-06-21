2a0f2e9224d5298a1b8e3b9ae395a9304381fd45
Mustafa Uçar <mustafa19071907mustafa@gmail.com>
2023-06-21 18:32:55 +0300

    Get_product_detail for quck view

--- product/urls.py
+++ product/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('',views.index,name="index"),
     path('addcomment/<int:id>', views.addcomment, name='addcomment'),
+    path('Get_product_detail/', views.Get_product_detail, name='Get_product_detail')
 ]
