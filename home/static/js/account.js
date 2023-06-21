e2ac701f53052426d108432a1eafd82bcd6bffff
Mustafa Uçar <mustafa19071907mustafa@gmail.com>
2023-06-21 03:30:09 +0300

    Add functionality of list_group_items buttons

--- home/static/js/account.js
+++ home/static/js/account.js
@@ -42,3 +42,26 @@ for (var i = 0; i < Form.length; i++) {
         })
     })
 }
+
+
+// get just list_group_items in id = logged-in-myAccount <section></section>
+var list_group_items = document.querySelectorAll("#logged-in-myAccount .list-group-item");
+
+list_group_items.forEach(function(item) {
+    item.addEventListener("click", function() {
+        var active = document.querySelector("#logged-in-myAccount .list-group-item.active");
+        active.classList.remove("active");
+        item.classList.add("active");
+        // for loggedin Users Account , Order infos id show in this section
+        showDiv = item.dataset.show;
+        // seçileni göster
+        document.getElementById(showDiv).style.display = "block";     
+        // diğerlerini gizle item haric diğerlerini gizle
+        var others = document.querySelectorAll("#logged-in-myAccount .list-group-item:not(.active)");
+        others.forEach(function(other) {
+            hideDiv = other.dataset.show;
+            document.getElementById(hideDiv).style.display = "none";
+        })
+        
+    })
+})
