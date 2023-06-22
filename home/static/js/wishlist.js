77e5bd2070da6a5a7506035ae348ebf721c052bc
Mustafa Uçar <mustafa19071907mustafa@gmail.com>
2023-06-22 15:45:34 +0300

    wish list fix

--- home/static/js/wishlist.js
+++ home/static/js/wishlist.js
@@ -1,18 +1,16 @@
-wishListBtn = document.querySelectorAll(".wishListBtn");
 
-wishListBtn.forEach((btn) => {
-    btn.addEventListener("click", (e) => {
-        let productid = btn.dataset.productid;
-        let action = btn.dataset.action;
-        if (user == "AnonymousUser") {
-            console.error("not logedin");
-        }
-        else {
-            wishListUpdateItem(e, productid, action)
-        }
-    })
-})
+function WishListBtn(e) {
 
+    let productid = e.dataset.productid;
+    let action = e.dataset.action;
+    if (user == "AnonymousUser") {
+        console.error("not logedin");
+    }
+    else {
+        wishListUpdateItem(e, productid, action);
+    }
+
+}
 
 function wishListUpdateItem(e, productid, action) {
     fetch("/order/wishListUpdateItem/", {
@@ -30,6 +28,5 @@ function wishListUpdateItem(e, productid, action) {
     }
     ).then((data) => {
         console.log(data);
-        location.reload()
     })
 }
